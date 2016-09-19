8c79b9255c55fe01445fa4dab8b27ccaadfbb772
vilumkris <vilumkris@gmail.com>
2016-09-19 20:58:03 +0300

    change User.ts

--- web/src/main/resources/public/src/shared/models/User.ts
+++ web/src/main/resources/public/src/shared/models/User.ts
@@ -11,22 +11,23 @@ export class User {
     activated:boolean;
     apartment:any;
     role:string;
-     
-      constructor() {
-     this.userId=new Number();
-    this.firstName=new String();
-    this.lastName=new String();
-    this.birthDate=new String();
-    this.email=new String();
-    this.phoneNumber=new String();
-    this.gender=new String();
-    // this.password=new Number();
-    this.activated=new Boolean();
-    this.role=new String();
-  }
-    constructor(userItem?:{userId:number,firstName:string,lastName:string,birthDate:string,email:string,phoneNumber:string,gender:string,activated:boolean,role:string,apartment:any}) {
-        if(userItem) {
-            Object.assign(this,userItem);
+
+    constructor() {
+        this.userId = new Number();
+        this.firstName = new String();
+        this.lastName = new String();
+        this.birthDate = new String();
+        this.email = new String();
+        this.phoneNumber = new String();
+        this.gender = new String();
+        this.password = new Number();
+        this.activated = new Boolean();
+        this.role = new String();
+    }
+
+    constructor(userItem?:{userId:number,firstName:string,lastName:string,birthDate:string,email:string,phoneNumber:string,gender:string,password:number,activated:boolean,role:string,apartment:any}) {
+        if (userItem) {
+            Object.assign(this, userItem);
         }
     }
 }
