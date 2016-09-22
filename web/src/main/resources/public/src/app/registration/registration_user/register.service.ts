7520849a44de0e5455781d97dd9279ab7826ff90
Kostetsky Roma <KostetskyRoma@yandex.ru>
2016-09-22 10:20:28 +0300

    Changed structure folders in registration

--- web/src/main/resources/public/src/app/registration/registration_user/register.service.ts
+++ web/src/main/resources/public/src/app/registration/registration_user/register.service.ts
@@ -1,19 +1,60 @@
 import {Injectable} from "@angular/core";
-import {HTTP_PROVIDERS, Http,Headers,Response} from "@angular/http";
+import {HTTP_PROVIDERS, Http, Headers, Response, RequestOptions} from "@angular/http";
 import {User} from "../../../shared/models/User";
+import {Osbb} from "../../../shared/models/osbb";
 import {Observable} from 'rxjs/Observable';
 import ApiService = require("../../../shared/services/api.service");
-
+import RegistrationComponent = require("./registration.component");
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 @Injectable()
-export class RegisterService{
-    private _pathUrl=ApiService.serverUrl + '/registration';
-    constructor(private http:Http){
-    }
-    sendUser(user:User){
-        console.log(user);
-        let headers=new Headers({'Content-Type':'application/json'});
-    return this.http.post(this._pathUrl,JSON.stringify(user),{headers:headers});
+export class RegisterService {
+
+    public _pathUrlForOsbb = ApiService.serverUrl + '/registration/osbb';
+    public _pathUrlForUser = ApiService.serverUrl + '/registration/';
+
+    constructor(private http: Http) {
+        
     }
 
-    
+    addOSBB(osbb: Osbb): Observable<any> {
+        let headers = new Headers({ 'Content-Type': 'application/json' }); 
+        let options = new RequestOptions({ headers: headers });
+        return  this.http.post(this._pathUrlForOsbb,osbb, options).map((res: Response) => res.json());
+    }
+     addUser(user: User): Observable<any> {
+        let headers = new Headers({ 'Content-Type': 'application/json' }); 
+        let options = new RequestOptions({ headers: headers });
+        return  this.http.post(this._pathUrlForUser,user, options).map((res: Response) => res.json());
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ // sendOsbb(osbb: Osbb) {
+    //     console.log(osbb);
+    //     let headers = new Headers({ 'Content-Type': 'application/json' });
+    //     return this.http.post(this._pathUrl1, JSON.stringify(osbb), { headers: headers });
+    // }
+
+    // sendUser(user:User){
+    //     console.log(user);
+    //     let headers=new Headers({'Content-Type':'application/json'});
+    // return this.http.put(this._pathUrl2,JSON.stringify(user),{headers:headers});
+    //}
+
+    // sendU(user: User){
+    //     console.log()
+    //     return this.userId;
+    // }
--- web/src/main/resources/public/src/app/registration/registration_user/registration.component.ts
+++ web/src/main/resources/public/src/app/registration/registration_user/registration.component.ts
@@ -1,75 +1,103 @@
 import {Component, OnInit, Output} from '@angular/core'
 import {EventEmitter} from '@angular/core';
 import {User} from "../../../shared/models/User";
+import {Osbb} from "../../../shared/models/osbb";
 import {RegisterService} from "./register.service";
 import {ROUTER_DIRECTIVES, Router} from "@angular/router";
 import MaskedInput from 'angular2-text-mask';
-import emailMask from 'node_modules/text-mask-addons/dist/emailMask.js'
+import emailMask from 'node_modules/text-mask-addons/dist/emailMask.js';
+import {GoogleplaceDirective} from "./googleplace.directive";
+import {TimerWrapper} from '@angular/core/src/facade/async';
+
+
 @Component({
     selector: 'app-register',
     templateUrl: 'src/app/registration/registration_user/registration.html',
-    styleUrls:['assets/css/registration/registration.css'],
-    providers:[RegisterService],
-    directives: [ROUTER_DIRECTIVES,MaskedInput]
+    styleUrls: ['assets/css/registration/registration.css'],
+    providers: [RegisterService],
+    directives: [ROUTER_DIRECTIVES, MaskedInput, GoogleplaceDirective]
 })
-export class RegistrationComponent implements OnInit  {
-    options = ['Приєднатись до існуючого ОСББ','Створити нове ОСББ'];
-    newUser:User=new User();
+export class RegistrationComponent implements OnInit {
+    registered: boolean;
+    options = ['Приєднатись до існуючого ОСББ', 'Створити нове ОСББ'];
+    newUser: User = new User();
+    newOsbb: Osbb = new Osbb();
     public emailMask = emailMask;
-    public textmask=[/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/,/[A-zА-яІ-і]/];
-    public phoneMask=['(', /[0]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
-    confirmPassword:number="";
-    error:boolean=false;
-    errorConfirm:boolean=false;
-    errorMsg="";
-    registered:boolean;
-    constructor(private registerService:RegisterService,private _router: Router) {
-    this.newUser.password="";
-
-    }
-
-    onSubmit(){
-        this.registerService.sendUser(this.newUser).subscribe(
-            data => {
-               this.registered=true;
-                this.newUser=new User();
-                console.log(this.newUser)
-            },
-            error=>console.log(error)
-        );
+    public textmask = [/[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/, /[A-zА-яІ-і]/];
+    public phoneMask = ['(', /[0]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/];
+    confirmPassword: number = "";
+    error: boolean = false;
+    errorConfirm: boolean = false;
+    errorMsg = "";
+    constructor(private registerService: RegisterService, private _router: Router) {
+         this.newUser.password = "";
+         this.newUser.activated = true;
+         this.newOsbb.creationDate = new Date;
     }
     
-    confirmPass(){
-    this.error=false;
-    var password=this.confirmPassword;
-    var password2=this.newUser.password;
-   if(password.length!=0){
-     if(password!=password2)
-    { 
-   
-       this.errorMsg="Passwords don't match. Please try again";
-        this.errorConfirm=true;
-        this.confirmPassword="";
-    return;
+    onSubmitUser(){
+        this.registered = true;
     }
-   }
- if(password2.length<4||password2.length>16)
-    {
-     this.errorMsg="Password cannot be shorter than 4 and longer than 16 characters";
-    this.error=true;
-    this.errorConfirm=false;
-    }else{this.errorConfirm=false;
+
+    onSubmitOsbb(){
+        this.Sender();
+        setTimeout(() => {
+        this._router.navigate(['/login']);
+        },2000);
     }
+
+    Sender (){
+        this.registerService.addUser(this.newUser).subscribe(
+                data => {
+                    this.newUser=data;  
+                    this.newOsbb.creator = this.newUser; 
+                    this.registerService.addOSBB(this.newOsbb).subscribe(data=>{
+                        console.log(data)
+                    });
+                    console.log(data);
+                error => console.log("Error HTTP Post Service");
+                () => console.log("Job Done Osbb!");
+                });        
+    }
+
+
+
+    public address: Object;
+    getAddress(place: Object) {
+        this.newOsbb.address = place['formatted_address'];
+        var location = place['geometry']['location'];
+        var lat = location.lat();
+        var lng = location.lng();
+        console.log("Address Object", place);
+    }
+    confirmPass() {
+        this.error = false;
+        var password = this.confirmPassword;
+        var password2 = this.newUser.password;
+        if (password.length != 0) {
+            if (password != password2) {
+                this.errorMsg = "Passwords don't match. Please try again";
+                this.errorConfirm = true;
+                this.confirmPassword = "";
+                return;
+            }
+        }
+        if (password2.length < 4 || password2.length > 16) {
+            this.errorMsg = "Password cannot be shorter than 4 and longer than 16 characters";
+            this.error = true;
+            this.errorConfirm = false;
+        } else {
+            this.errorConfirm = false;
+        }
     }
 
     onUserClick(status) {
-        if (status == this.options[1]){
+        if (status == this.options[1]) {
             console.log('CreateNew');
-            this._router.navigate(['/registration/osbb']);
         }
-        else if (status == this.options[0]){
+        else if (status == this.options[0]) {
             console.log('JoinToExist');
             this._router.navigate(['/join/osbb']);
-        } 
-     }
+        }
+    }
 }
