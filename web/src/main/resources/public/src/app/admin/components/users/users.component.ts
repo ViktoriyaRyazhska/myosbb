1002cd8beba1446f8e50ea6738b1f49991b99709
Oleg Kykena <ikena7@gmail.com>
2016-11-14 18:39:58 +0200

    Make admin's able to create user with any role

--- web/src/main/resources/public/src/app/admin/components/users/users.component.ts
+++ web/src/main/resources/public/src/app/admin/components/users/users.component.ts
@@ -1,26 +1,64 @@
-import {Component, OnInit} from '@angular/core'
-import {User} from '../../../../shared/models/User';
-import {UsersService} from "./users.service";
-import {TranslatePipe} from "ng2-translate";
-import {CapitalizeFirstLetterPipe} from "../../../../shared/pipes/capitalize-first-letter";
-import {Router} from '@angular/router';
-import {REACTIVE_FORM_DIRECTIVES, FormBuilder, Validators} from '@angular/forms';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../../../../shared/models/User';
+import { Role } from '../../../../shared/models/role';
+import { Osbb, IOsbb } from "../../../../shared/models/osbb";
+import { IHouse } from "../../../../shared/models/House";
+import { IApartment } from "../../../../shared/models/apartment.interface";
+import { UserRegistration } from '../../../../shared/models/user_registration';
+import { OsbbRegistration } from "../../../../shared/models/osbb_registration";
+import { UsersService} from "./users.service";
+import { RegisterService } from "../../../registration/registration_user/register.service";
+// import { RoleService } from "../role/role.service";
+import { RegistrationComponent } from "../../../registration/registration_user/registration.component";
+import { TranslatePipe } from "ng2-translate";
+import { CapitalizeFirstLetterPipe } from "../../../../shared/pipes/capitalize-first-letter";
+import { Router } from '@angular/router';
+import { REACTIVE_FORM_DIRECTIVES, FormBuilder, Validators, NgForm } from '@angular/forms';
+import { SELECT_DIRECTIVES } from "ng2-select";
 
 
-@Component({
+@Component({ 
     selector: 'my-users',
     templateUrl: 'src/app/admin/components/users/users.table.html',
-    providers: [UsersService],
+    providers: [UsersService, RegisterService],
     styleUrls: ['src/app/admin/components/users/users.css'],
     pipes: [TranslatePipe, CapitalizeFirstLetterPipe],
-    directives: [REACTIVE_FORM_DIRECTIVES]
+    directives: [REACTIVE_FORM_DIRECTIVES, RegistrationComponent,  SELECT_DIRECTIVES]
 })
 export class UsersComponent implements OnInit {
+    
     userList:User[];
     userForm:any;
+    roles: Array<string> = [];
+    userMy: UserRegistration = new UserRegistration();
+    osbbMy: OsbbRegistration = new OsbbRegistration();
+    private isSelectedOsbb: boolean = false;
+    private isSelectedHouse: boolean = false;
+    private isSelectedApartment: boolean = false;
+    private IsRegistered: boolean;
+    private IsRegisteredOsbb: boolean;
+    private isJoinedOsbb: boolean;
+    private roleList: Role[] = [];
+    private osbbList: IOsbb[] = [];
+    private houseList: IHouse[] = [];
+    private apartmentList: IApartment[] = [];
+    private osbb: Array<string> = [];
+    private houses: Array<string> = [];
+    private apartment: Array<string> = [];
+    genders = [
+        'male',
+        'female'
+    ];
+    options = [
+        'Створити нове ОСББ',
+        'Приєднатись до існуючого ОСББ'
+        ];
+    
 
-    constructor(private _userService:UsersService, private router:Router, private formBuilder:FormBuilder) {
+    constructor(private _userService:UsersService, private router:Router, private formBuilder:FormBuilder,
+        private registerService: RegisterService) {
         console.log('constructore');
+        this.userMy.activated = true;
         this.userList = [];
         this.userForm = this.formBuilder.group({
             'firstName': ['', Validators.required],
@@ -36,6 +74,8 @@ export class UsersComponent implements OnInit {
     ngOnInit():any {
         console.log('init');
         this._userService.getAllUsers().subscribe(data => this.userList = data, error=>console.error(error));
+         this.listAllOsbb();
+         this.listAllRoles();
         console.log('get out of service');
     }
 
@@ -47,9 +87,10 @@ export class UsersComponent implements OnInit {
         this._userService.deleteUser(user).subscribe(()=>this.userList.splice(this.userList.indexOf(user, 0), 1));
     }
 
-    saveUser() {
-        let user:User = this.userForm;
-        this._userService.saveUser(user).subscribe((data)=>this.userList.push(data));
+    saveUser(form: NgForm ) {
+        console.log(this.userMy);
+//        let user:User = this.userForm;
+        this._userService.saveUser(this.userMy).subscribe((data)=>this.userList.push(data));
     }
 
 
@@ -66,5 +107,209 @@ export class UsersComponent implements OnInit {
     toUser(id:number){
         this.router.navigate(['admin/user', id]);
     }
+
+    onSubmitUser(status) {
+        if (status == this.options[1]) {
+            console.log('CreateNew');
+            this.IsRegistered = false;
+            this.IsRegisteredOsbb = true;
+            this.isJoinedOsbb = false;
+        }
+        else if (status == this.options[0]) {
+            console.log('JoinToExist');
+            this.IsRegistered = false;
+            this.isJoinedOsbb = true;
+            this.IsRegisteredOsbb = false;
+        }
+    }
+
+    selectedOsbb(value: any) {
+        this.isSelectedOsbb = true;
+        console.log('select osbb: ', value);
+        let selectedOsbb: Osbb = this.getOsbbByName(value.text);
+        this.userMy.osbbId = selectedOsbb.osbbId;
+        this.listAllHousesByOsbb(this.userMy.osbbId);
+    }
+
+
+    selectedHouse(value: any) {
+        console.log('select house: ' + value);
+        this.isSelectedHouse = true;
+        let houseId = this.getHouseIdByName(value.text);
+        console.log('select houseId: ' + houseId);
+        this.listAllApartmentsByHouse(houseId);
+    }
+
+
+    selectedApartment(value: any) {
+        this.isSelectedApartment = true;
+        let selectedApartmentID: number = this.getApartmentByApartmentNumber(value.text);
+        this.userMy.apartmentId = selectedApartmentID;
+        console.log('select apartment: ' + selectedApartmentID);
+        console.log(JSON.stringify(this.userMy));
+    }
+
+    selectedRole(value: any) {
+        let selectedRole: Role = this.getRoleByName(value.text);
+        this.userMy.role = selectedRole.roleId;
+    }
+
+    listAllOsbb() {
+        this.registerService.getAllOsbb()
+            .subscribe((data)=> {
+                this.osbbList = data;
+                this.osbb = this.fillOsbb();
+                console.log('all osbb names', this.osbb);
+            });
+    }
+
+    listAllRoles() {
+        this._userService.listAllRoles()
+        .subscribe((data) => {
+            this.roleList = data;
+            this.roles = this.fillRoles();
+            console.log('all role names', this.roles);
+        });
+    }
+
+    
+
+    // listAllHouses() {
+    //     this.registerService.getAllHouses()
+    //         .subscribe((data)=> {
+    //             this.houseList = data;
+    //             this.houses = this.fillHouses();
+    //             console.log('all houses', this.houses);
+    //         }, (error)=> {
+    //             this.handleErrors(error)
+    //         });
+    // }
+
+
+    listAllHousesByOsbb(id: number) {
+
+        this.registerService.getAllHousesByOsbb(id)
+            .subscribe((data)=> {
+                    this.houseList = data;
+                    this.houses = this.fillHouses();
+                    console.log('all houses', this.houses);
+                })
+
+    }
+
+    // listAllApartments() {
+    //     this.registerService.getAllApartments()
+    //         .subscribe((data)=> {
+    //             this.apartmentList = data;
+    //             this.apartment = this.fillApartment();
+    //             console.log('all apartment', this.apartment);
+    //         }, (error)=> {
+    //             this.handleErrors(error)
+    //         });
+    // }
+
+
+    listAllApartmentsByHouse(houseId: number) {
+        this.registerService.getAllApartmentsByHouse(houseId)
+            .subscribe((data)=> {
+                this.apartmentList = data;
+                this.apartment = this.fillApartment();
+                console.log('all apartment', this.apartment);
+            });
+    }
+
+    getOsbbByName(name: string): Osbb {
+        let selectedOsbb: Osbb = new Osbb();
+        for (let osbb of this.osbbList) {
+            if (osbb.name.match(name)) {
+                selectedOsbb = osbb;
+                break;
+            }
+        }
+        return selectedOsbb;
+    }
+
+    getRoleByName(name: string): Role {
+        let selectedRole: Role = new Role();
+        for (let role of this.roleList) {
+            if (role.name.match(name)) {
+                selectedRole = role;
+                break;
+            }
+        }
+        return selectedRole;
+    }
+
+    getHouseIdByName(name: string): number {
+        let houseId = 0;
+        for (let house of this.houseList) {
+            if (house.street.match(name)) {
+                houseId = house.houseId;
+                break;
+            }
+        }
+        return houseId;
+    }
+
+
+    getApartmentByApartmentNumber(apartmentNumber: string): number {
+        let apartmentID: number = 0;
+        let apNumber = +apartmentNumber;
+        for (let ap of this.apartmentList) {
+            if (ap.number === apNumber) {
+                apartmentID = ap.apartmentId
+                break;
+            }
+        }
+
+        return apartmentID;
+    }
+
+
+    fillOsbb(): string[] {
+        let tempArr: string[] = [];
+        for (let osbbObject of this.osbbList) {
+            tempArr.push(osbbObject.name);
+        }
+        console.log(tempArr)
+        return tempArr;
+    }
+
+    fillOsbbById(): number[] {
+        let tempArr: number[] = [];
+        for (let osbbObject of this.osbbList) {
+            tempArr.push(osbbObject.osbbId);
+        }
+        console.log(tempArr)
+        return tempArr;
+    }
+
+    fillHouses(): string[] {
+        let tempArr: string[] = [];
+        for (let houseObject of this.houseList) {
+            tempArr.push('' + houseObject.street);
+        }
+        console.log(tempArr)
+        return tempArr;
+    }
+
+    fillApartment(): string[] {
+        let tempArr: string[] = [];
+        for (let apartmentObject of this.apartmentList) {
+            tempArr.push('' + apartmentObject.number)
+        }
+        console.log(tempArr)
+        return tempArr;
+    }
+
+    fillRoles(): string[] {
+        let tempArr: string[] = [];
+        for (let roleObject of this.roleList) {
+            tempArr.push(roleObject.name);
+        }
+        console.log(tempArr)
+        return tempArr;
+    }
+
 
 }
---
+++ web/src/main/resources/public/src/shared/models/role.ts
@@ -0,0 +1,10 @@
+import assign = require("core-js/library/fn/object/assign");
+
+export class Role {
+    roleId: number;
+    name: string;
+
+    constructor() {
+       
+    }
+}
