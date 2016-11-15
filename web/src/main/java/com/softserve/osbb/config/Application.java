91bfa9bd9a0c61f6a23f4d8af5a39a5242e77115
KostyantynPanchenko <kostyantyn.panchenko@gmail.com>
2016-11-15 21:53:06 +0200

    Refactored configuration classes

--- web/src/main/java/com/softserve/osbb/config/Application.java
+++ web/src/main/java/com/softserve/osbb/config/Application.java
@@ -1,25 +1,40 @@
+/*
+ * Project “OSBB” – a web-application which is a godsend for condominium head, managers and 
+ * residents. It offers a very easy way to manage accounting and residents, events and 
+ * organizational issues. It represents a simple design and great functionality that is needed 
+ * for managing. 
+ */
 package com.softserve.osbb.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 /**
+ * Initializes this Spring Boot application.
+ * 
  * Created by nataliia on 24.07.16.
+ * @version 1.1
+ * @since 15.11.2016 
  */
-
-@Configuration
-@EnableAutoConfiguration
-@SpringBootApplication
-@ComponentScan(basePackages = {"com.softserve.osbb"})
+@SpringBootApplication(scanBasePackages = "com.softserve.osbb")
 public class Application extends SpringBootServletInitializer {
 
+    /**
+     * Bootstraps Spring Boot application.
+     * @param args CLI arguments.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+    /**
+     * {@inheritDoc}}
+     */
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application;
+        return application.sources(Application.class);
     }
 
 }
--- web/src/main/java/com/softserve/osbb/config/WebSecurityConfiguration.java
+++ web/src/main/java/com/softserve/osbb/config/WebSecurityConfiguration.java
@@ -1,3 +1,9 @@
+/*
+ * Project “OSBB” – a web-application which is a godsend for condominium head, managers and 
+ * residents. It offers a very easy way to manage accounting and residents, events and 
+ * organizational issues. It represents a simple design and great functionality that is needed 
+ * for managing. 
+ */
 package com.softserve.osbb.config;
 
 import com.softserve.osbb.service.utils.CustomUserDetailsService;
@@ -15,9 +21,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
+ * Web security configuration class.
+ * 
  * Created by cavayman on 30.08.2016.
+ * @version 1.1
+ * @since 15.11.2016
  */
-
 @Configuration
 @ComponentScan("com.softserve.osbb.service")
 @EnableWebSecurity
@@ -29,25 +38,36 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    /**
+     * Configure instance of UserDetailService to use specific password encoder mechanism.
+     */
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder);
     }
 
-
+    /**
+     * Initializes authentication manager.
+     */
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
+    /**
+     * Sets Bcrypt encoder to be used in application.
+     * @return PAsswordEncoder instance.
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-
+    /**
+     * Configure access to specified urls.
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
