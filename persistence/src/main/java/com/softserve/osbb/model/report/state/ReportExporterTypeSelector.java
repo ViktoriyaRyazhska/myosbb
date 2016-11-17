6ee2c8fbaca83bdfe6a12ccf44c3f783d0774f36
KostyantynPanchenko <kostyantyn.panchenko@gmail.com>
2016-11-17 19:57:28 +0200

    Deleted extra classes

--- persistence/src/main/java/com/softserve/osbb/model/report/state/ReportExporterTypeSelector.java
+++
@@ -1,9 +0,0 @@
-package com.softserve.osbb.model.report.state;
-
-import com.softserve.osbb.model.report.ReportExporter;
-
-/**
- * Created by nazar.dovhyy on 05.09.2016.
- */
-public interface ReportExporterTypeSelector extends Creator<ReportExporter> {
-}
