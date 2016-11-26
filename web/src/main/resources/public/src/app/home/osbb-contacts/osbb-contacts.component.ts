7add28e800432e471f488c4d6fa526eace25efc5
KostyantynPanchenko <kostyantyn.panchenko@gmail.com>
2016-11-26 16:20:24 +0200

    Added osbb-contacts.component

---
+++ web/src/main/resources/public/src/app/home/osbb-contacts/osbb-contacts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'user-menu-osbb-contacts',
+    templateUrl: 'src/app/home/osbb-contacts/osbb-contacts.html',
+    styleUrls: ['src/app/home/home_wall/home.wall.css']
+})
+export class OsbbContactsComponent {
+    
+}
