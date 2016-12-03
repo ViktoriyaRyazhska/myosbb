8af614acbe2973412135309d5ec60030c5aafa57
KostyantynPanchenko <kostyantyn.panchenko@gmail.com>
2016-12-03 17:39:43 +0200

    Renamed components selectors

--- web/src/main/resources/public/src/app/home/osbb-docs-and-reports/financial-statements/financial-statements.component.ts
+++ web/src/main/resources/public/src/app/home/osbb-docs-and-reports/financial-statements/financial-statements.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { TranslatePipe } from 'ng2-translate';
 
 @Component({
-    selector: 'user-menu-osbb-docs-and-reports',
+    selector: 'financial-statements',
     templateUrl: 'src/app/home/osbb-docs-and-reports/financial-statements/financial-statements.html',
     styleUrls: ['src/app/home/osbb-contacts/osbb-contacts.css'],
     pipes:[TranslatePipe]
--- web/src/main/resources/public/src/app/home/osbb-docs-and-reports/regulations/regulations.component.ts
+++ web/src/main/resources/public/src/app/home/osbb-docs-and-reports/regulations/regulations.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { TranslatePipe } from 'ng2-translate';
 
 @Component({
-    selector: 'user-menu-osbb-docs-and-reports',
+    selector: 'osbb-regulations',
     templateUrl: 'src/app/home/osbb-docs-and-reports/regulations/regulations.html',
     styleUrls: ['src/app/home/osbb-contacts/osbb-contacts.css'],
     pipes:[TranslatePipe]
