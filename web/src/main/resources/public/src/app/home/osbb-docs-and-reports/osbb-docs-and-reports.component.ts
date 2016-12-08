3826a7f1704452f325cffd392a9febc278997f4e
KostyantynPanchenko <kostyantyn.panchenko@gmail.com>
2016-12-08 12:00:15 +0200

    Added toaster service

--- web/src/main/resources/public/src/app/home/osbb-docs-and-reports/osbb-docs-and-reports.component.ts
+++ web/src/main/resources/public/src/app/home/osbb-docs-and-reports/osbb-docs-and-reports.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ROUTER_DIRECTIVES, Router, ActivatedRoute } from "@angular/router";
 import { TranslatePipe } from 'ng2-translate';
+import { ToasterContainerComponent, ToasterService, ToasterConfig } from 'angular2-toaster/angular2-toaster';
 
 import { Subscription } from 'rxjs/Subscription';
 import { User } from '../../../shared/models/User';
@@ -14,8 +15,8 @@ import { Folder } from './folder-manager/Folder';
     templateUrl: 'src/app/home/osbb-docs-and-reports/osbb-docs-and-reports.html',
     styleUrls: ['src/app/home/osbb-contacts/osbb-contacts.css', 
                 'src/app/home/osbb-docs-and-reports/osbb-docs-and-reports.css'],
-    directives: [ROUTER_DIRECTIVES],
-    providers: [FolderService],
+    directives: [ROUTER_DIRECTIVES, ToasterContainerComponent],
+    providers: [FolderService, ToasterService],
     pipes:[CapitalizeFirstLetterPipe, TranslatePipe]
 })
 export class OsbbDocumentsAndReportsComponent implements OnInit, OnDestroy {
@@ -30,7 +31,8 @@ export class OsbbDocumentsAndReportsComponent implements OnInit, OnDestroy {
     constructor(private userService: CurrentUserService,
                 private router: Router, 
                 private activatedRoute: ActivatedRoute, 
-                private folderService: FolderService) 
+                private folderService: FolderService,
+                private toasterService: ToasterService) 
     {
         this.subscription =  activatedRoute.params.subscribe(
             (params) => {
@@ -62,18 +64,65 @@ export class OsbbDocumentsAndReportsComponent implements OnInit, OnDestroy {
     }
 
     private createNewFolder(name: string) {
-        console.log('Saving folder ' + this.newFolder.name + ' with parentId=' + this.parentId);
-        this.folderService.save(name, this.parentId)
-            .subscribe(
-                data => {
-                    this.newFolder = data;
-                    console.log('Successfully saved: name=' + this.newFolder.name + ', id=' + this.newFolder.id);
-                    this.initFolders();
-                },
-                error => console.log(error)
-            );
+        if (this.folderExist(name)) {
+            this.toasterService.pop('error', 'Папка ' + name + ' вже існує!');
+        } else {
+            console.log('Saving folder ' + this.newFolder.name + ' with parentId=' + this.parentId);
+            this.folderService.save(name, this.parentId)
+                .subscribe(
+                    data => {
+                        this.newFolder = data;
+                        console.log('Successfully saved: name=' + this.newFolder.name + ', id=' + this.newFolder.id);
+                        this.initFolders();
+                        this.toasterService.pop('success', 'Папка ' + name +' була успішно створена');                        
+                    },
+                    error => console.log(error)
+                );
+        }
+        this.newFolder = new Folder();
     }
 
+    private deleteFolder(id: number) {        
+        if (confirm()) {
+            this.folderService.delete(id)
+                .subscribe(
+                    data => {                    
+                        console.log('Successfully deleted');
+                        this.initFolders();
+                        this.toasterService.pop('success', 'Папка була успішно видалена!');
+                    },
+                    error => {
+                        console.log(error);
+                        this.toasterService.pop('error', 'Не зміг видалити папку!');
+                    }
+                );
+        }
+    }
+
+    private updateFolder() {       
+        if (this.folderExist(this.editableFolder.name)) {
+            this.toasterService.pop('error', 'Папка ' + this.editableFolder.name + ' вже існує!');
+        } else {
+            this.folderService.update(this.editableFolder)
+                .subscribe(
+                    data => {                    
+                        console.log('Successfully updated');
+                        this.editableFolder = data;
+                        this.initFolders();
+                    },
+                    error => console.log(error)
+            );    
+        }    
+    }
+
+    private getFolder(id: number) {
+        this.folderService.getFolder(id)
+            .subscribe(
+                data => this.editableFolder = data,                    
+                error => console.log(error)                
+            );
+    }
+    
     private checkParentId() {
         if (!this.parentId) {
             this.parentId = 1;
@@ -87,36 +136,17 @@ export class OsbbDocumentsAndReportsComponent implements OnInit, OnDestroy {
         console.log(this.editMode);
     }
 
-    private deleteFolder(id: number) {        
-        if (confirm()) {
-            this.folderService.delete(id)
-                .subscribe(
-                    data => {                    
-                        console.log('Successfully deleted');
-                        this.initFolders();
-                    },
-                    error => console.log(error)
-                );
+    private folderExist(name: string): boolean {
+        var exist: boolean = false;
+        var i: number;
+        var size = this.folders.length;
+
+        for (i = 0; i < size; i++) {
+            if (this.folders[i].name.toUpperCase() == name.toUpperCase()) {                
+                exist = true;
+                break;
+            }
         }
-    }
-
-    private updateFolder() {       
-        this.folderService.update(this.editableFolder)
-            .subscribe(
-                data => {                    
-                    console.log('Successfully updated');
-                    this.editableFolder = data;
-                    this.initFolders();
-                },
-                error => console.log(error)
-        );        
-    }
-
-    private getFolder(id: number) {
-        this.folderService.getFolder(id)
-            .subscribe(
-                data => this.editableFolder = data,                    
-                error => console.log(error)                
-            );
+        return exist;
     }
 }
