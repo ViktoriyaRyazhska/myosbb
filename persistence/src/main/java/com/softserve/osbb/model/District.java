4fc43421de73a381bd9a3e5fa9f844fe4b7d41c5
YuriPushchalo <jepseo@gmail.com>
2016-12-23 11:45:02 +0200

    DISTRICT entity added

---
+++ persistence/src/main/java/com/softserve/osbb/model/District.java
@@ -0,0 +1,57 @@
+package com.softserve.osbb.model;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+/**
+ * Created by Yuri Pushchalo on 23.12.2016.
+ */
+@Entity
+@Table(name = "districts")
+public class District implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String name;
+    private City city;
+
+    public District(){}
+
+    public District(String name, City city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+	@ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+}
