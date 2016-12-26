df8b02e1abc8c387ce4817a4798a649c11e2c284
YuriPushchalo <jepseo@gmail.com>
2016-12-26 02:46:10 +0200

    Address service modified: DISTRICT function added

--- service/src/main/java/com/softserve/osbb/service/impl/AddressServiceImpl.java
+++ service/src/main/java/com/softserve/osbb/service/impl/AddressServiceImpl.java
@@ -2,9 +2,11 @@ package com.softserve.osbb.service.impl;
 
 import com.softserve.osbb.model.Region;
 import com.softserve.osbb.model.City;
+import com.softserve.osbb.model.District;
 import com.softserve.osbb.model.Street;
 import com.softserve.osbb.repository.RegionRepository;
 import com.softserve.osbb.repository.CityRepository;
+import com.softserve.osbb.repository.DistrictRepository;
 import com.softserve.osbb.repository.StreetRepository;
 import com.softserve.osbb.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +31,9 @@ public class AddressServiceImpl implements AddressService {
 	@Autowired
 	private StreetRepository streetRepository;
 
+	@Autowired
+	private DistrictRepository districtRepository;
+
 	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
 	@Override
 	public List<Region> getAllRegion() {
@@ -45,6 +50,18 @@ public class AddressServiceImpl implements AddressService {
 	@Override
 	public List<Street> getAllStreetsOfCity(Integer cityId) {
 		return streetRepository.findByCityOrderByName(cityRepository.findById(cityId));
+	}
+
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+	@Override
+	public List<District> getAllDistrictsOfCity(Integer cityId) {
+		return districtRepository.findByCityOrderByName(cityRepository.findById(cityId));
+	}
+
+	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+	@Override
+	public District getDistrictById(Integer id) {
+		return districtRepository.findById(id);
 	}
 
 	@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
