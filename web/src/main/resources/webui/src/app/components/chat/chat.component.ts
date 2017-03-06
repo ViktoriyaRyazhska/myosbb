d0e33429ca9475d4c010f14c87c76bedf1a0916a
RomanKarpliak <karpliakroman@gmail.com>
2017-03-06 20:48:30 +0200

    added chat component and it's html

--- web/src/main/resources/webui/src/app/components/chat/chat.component.ts
+++ web/src/main/resources/webui/src/app/components/chat/chat.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StompService } from './stomp.service';
 
 @Component({
     selector: 'chat',
     templateUrl: 'chat.component.html',
-    styleUrls: ['../../../assets/css/manager.page.layout.scss']
+    styleUrls: ['../../../assets/css/manager.page.layout.scss'],
+    providers: [StompService]
 })
 
-export class ChatComponent {
+export class ChatComponent implements OnInit {
   public title: string = `Chat`;
+  public inputField = '';
+  public serverResponse=[];
+  constructor(private _stompService: StompService) {
+  }
+
+  public ngOnInit(): void {
+   this._stompService.connect('ws://localhost:8080/myosbb/stompTest');
+   this.serverResponse= this._stompService._serverResponse;
+  }
+
+  public send(): void {
+    this._stompService.send(this.inputField);
+  }
+
+
 }
