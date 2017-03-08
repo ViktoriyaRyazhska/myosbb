2b5ef6fb633cc135bfcdfb06776d318b82b0624a
ohorodnyk <o.ohorodnyk@gmail.com>
2017-03-08 17:50:19 +0200

    this changes do not work

--- web/src/main/resources/webui/src/app/components/ticket/components/ticketAddFormComponent/ticket-add-form.component.ts
+++ web/src/main/resources/webui/src/app/components/ticket/components/ticketAddFormComponent/ticket-add-form.component.ts
@@ -7,26 +7,56 @@ import {
   ViewChild
 } from '@angular/core';
 
+import { DatePipe } from '@angular/common' ;
 import { ModalDirective } from "ng2-bootstrap/modal";
+import {Ticket, TicketState, ITicket} from "../../../../models/ticket.model";
+import {User} from "../../../../models/user.model";
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {TicketService} from "../../ticket.service";
+import { LoginService } from '../../../../shared/login/login.service';
 
 @Component({
     selector: 'ticket-add-form',
+    providers: [TicketService, FormBuilder, LoginService],
     templateUrl: './ticket-add-form.html',
     styleUrls: ['../../../../../assets/css/page.layout.scss']
 })
 
 export class TicketAddFormComponent {
-  @Input() ticket: string;
+  @Output() created:EventEmitter<Ticket>;
+  @Input() ticket: Ticket;
   @ViewChild('addModal')
   addModal: ModalDirective;
 
-  private submitAttempt: boolean;
-  private nameTicket: string = '';
+ private currentUser:User;
+ private userAssignArr:User[] = [];
+ private creatingForm:FormGroup;
+ private nameInput:FormControl;
+ private descriptionInput:FormControl;
+ private assignInput:FormControl;
+ private submitAttempt: boolean;
+ private nameTicket: string = '';
   private descriptionTicket: string = '';
   private assignTicket: string = '';
   private endTimeStr: string;
-
-  constructor() { }
+ private builder:FormBuilder;
+  constructor(private ticketService:TicketService,
+                private loginService:LoginService) {
+ 
+        this.currentUser =loginService.currentUser;
+        this.created = new EventEmitter<Ticket>();
+        this.ticket = new Ticket("","",TicketState.NEW,null);
+        this.submitAttempt = false;
+        this.nameInput = new FormControl('', Validators.required);
+        this.descriptionInput = new FormControl('', Validators.required);
+        this.assignInput = new FormControl('', Validators.required);
+        this.builder = new FormBuilder();
+        this.creatingForm =this.builder.group({
+            nameInput: this.nameInput,
+            descriptionInput: this.descriptionInput,
+            assignInput: this.assignInput
+        });
+   }
 
   public openAddModal() {
     this.addModal.show();
@@ -61,13 +91,37 @@ export class TicketAddFormComponent {
     this.endTimeStr = '';
   };
 
-  public onCreateTicket() {
+public onCreateTicket() {
+    if (this.nameInput.valid && this.descriptionInput.valid && this.assignInput.valid&&
+             !this.isEmptyDescription()&&!this.isEmptyName()&&this.isFindAssign() && this.isDeadLineCorrect()) {
+            this.created.emit(this.createTicket());
+            this.closeAddModal();
+        }
     this.closeAddModal();
   };
+      createTicket():Ticket {
+        let ticket = new Ticket(this.nameTicket, this.descriptionTicket, TicketState.NEW,null);
+        ticket.user = this.currentUser;
+       // ticket.attachments = this.attachments;
+        ticket.assigned = this.getAssignedId(this.assignTicket);
+        ticket.deadline = this.castDeadLineStringToDate();
+        return ticket;
 
-  public removeAttachment(attachments) {
-    if (this.addModal.isShown) {
-      let index = this.ticket.indexOf(attachments);
     }
-  };
+    getAssignedId(assign:string):User {
+        let str = assign.split(' ');
+        for (let i = 0; i < this.userAssignArr.length; i++) {
+            if (str[0] == this.userAssignArr[i].firstName && str[1] == this.userAssignArr[i].lastName) {
+                return this.userAssignArr[i];
+            }
+        } 
+    }
+        isDeadLineCorrect(): boolean {
+        let startTime = new Date();
+        let res = this.castDeadLineStringToDate().valueOf() - startTime.valueOf();
+        return res > 0;
+    }
+    castDeadLineStringToDate(): Date {
+        return new Date(this.endTimeStr);
+    };
 }
--- web/src/main/resources/webui/src/app/components/ticket/ticket.service.ts
+++ web/src/main/resources/webui/src/app/components/ticket/ticket.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import {
   Http,
-  Response
+  Response,
+  RequestOptions,
+  Headers
 } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -10,11 +12,15 @@ import 'rxjs/add/operator/toPromise';
 import { LoginService } from '../../shared/login/login.service';
 import { PageRequest } from '../../models/pageRequest.model';
 import { UrlListConfig } from '../../services/apiUrl.service';
+import {Ticket, TicketState, ITicket} from "../../models/ticket.model";
+import { LoginConstants } from '../../shared/login/login.constants';
+
 
 @Injectable()
 
 export class TicketService {
   private pageRequest = new PageRequest(1, 10, 'time', false);
+  private url:string =LoginConstants.Login.serverUrl + '/restful/ticket';
   constructor(
     private http: Http,
     private login: LoginService,
@@ -40,4 +46,17 @@ export class TicketService {
       .map((response) => response.json())
       .catch((error) => Observable.throw(error));
   };
+addTicket(ticket:ITicket):Promise<ITicket> {
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers });
+        return this.http.post(this.url, JSON.stringify(ticket), options)
+            .toPromise()
+            .then(res => res.json())
+            .catch(this.handleError);
+    };
+    private handleError(error:any):Promise<any> {
+        console.log('HandleError', error);
+        return Promise.reject(error.message || error);
+    };
+
 }
