dab65cfc206968b4798b6e5873bec1799c169168
nazar9501 <lvivnazar95@gmail.com>
2017-03-27 17:17:04 +0300

    update chat

--- web/src/main/resources/webui/src/app/components/chat/chat.component.ts
+++ web/src/main/resources/webui/src/app/components/chat/chat.component.ts
@@ -4,12 +4,15 @@ import { API_URL } from '../../../shared/models/localhost.config';
 import { Http, Response } from '@angular/http';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import { UsersService } from '../users/users.service';
+import { User } from '../../models/user.model';
+import { LoginService } from '../../shared/login/login.service';
 
 @Component({
     selector: 'chat',
     templateUrl: 'chat.component.html',
     styleUrls: ['../../../assets/css/manager.page.layout.scss', './chat.component.css'],
-    providers: [StompService]
+    providers: [StompService, UsersService]
 })
 
 export class ChatComponent implements OnInit {
@@ -17,13 +20,15 @@ export class ChatComponent implements OnInit {
   public inputField = '';
   public serverResponse=[];
   public messages: string[];
-  constructor(public route: ActivatedRoute, public http: Http, private _stompService: StompService) {
+  public currentUser: User;
+  constructor(public route: ActivatedRoute, public http: Http, private _stompService: StompService, public usersService: UsersService, private currentUserService: LoginService) {
   }
 
   public ngOnInit(): void {
    this._stompService.getMessages().subscribe((data) => {
                 this.messages = data;
             });
+   this.getCurrentUser();
    this._stompService.connect('ws://localhost:8080/myosbb/stompTest');
    this.serverResponse= this._stompService._serverResponse;
   }
@@ -32,5 +37,9 @@ export class ChatComponent implements OnInit {
     this._stompService.send(this.inputField);
     this.inputField='';
   }
+
+  public getCurrentUser() {
+        this.currentUser = this.currentUserService.getUser();
+    }
 
 }
